17b75ff41abb38606b2f03b716204b555825bbdb
Isuru Fernando <isuruf@gmail.com>
2018-11-20 14:05:18 -0600

    Use MSVC 2015

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,7 @@
 mkdir build
 cd build
 
-REM Configure step
-if "%ARCH%"=="32" (
-    set CMAKE_GENERATOR=Visual Studio 12 2013
-) else (
-    set CMAKE_GENERATOR=Visual Studio 12 2013 Win64
-)
-set CMAKE_GENERATOR_TOOLSET=v120_xp
-
-cmake -G "%CMAKE_GENERATOR%" -T "%CMAKE_GENERATOR_TOOLSET%" ^
+cmake -G "%CMAKE_GENERATOR%" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
     %SRC_DIR%
