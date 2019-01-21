b0c191024b1a8afcfc942f21d81c19808dcc2c68
Isuru Fernando <isuruf@gmail.com>
2019-01-21 15:51:39 -0600

    Use conda directory for vendor dir
    Also use OpenCL include directory for Apple

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,12 @@
 mkdir build
 cd build
 
+sed -i.bak icd_linux.c 's|/etc/OpenCL/vendors/|$PREFIX/etc/OpenCL/vendors/|g';
+
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j${CPU_COUNT}
 make install
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    mv $PREFIX/include/CL $PREFIX/include/OpenCL;
+fi
