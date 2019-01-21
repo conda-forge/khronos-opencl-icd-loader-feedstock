aacfe2bf960eff17907ee33f61a57555ea9a4bfb
Isuru Fernando <isuruf@gmail.com>
2019-01-21 16:12:26 -0600

    Sed from source directory

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
+sed -i.bak  "s|/etc/OpenCL/vendors/|$PREFIX/etc/OpenCL/vendors/|g" icd_linux.c;
+
 mkdir build
 cd build
-
-sed -i.bak  "s|/etc/OpenCL/vendors/|$PREFIX/etc/OpenCL/vendors/|g" icd_linux.c;
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j${CPU_COUNT}
