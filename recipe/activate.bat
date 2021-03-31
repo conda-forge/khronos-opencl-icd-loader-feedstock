08a1d51317cb3bdd6f8f8ea623dabb23b753fb3d
Isuru Fernando <isuruf@gmail.com>
2021-03-31 14:56:49 -0500

    fix windows activation

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,6 +1,14 @@
+@echo off
 set "OCL_ICD_FILENAMES_CONDA_BACKUP=%OCL_ICD_FILENAMES%"
 
-for %%f in (%PREFIX%\Library\etc\OpenCL\vendors\*.icd) do (
-  set /p dllname=< %%~f
-  set "OCL_ICD_FILENAMES=%OCL_ICD_FILENAMES%;%dllname%"
+echo|set /P ="" > %CONDA_PREFIX%\Library\etc\OpenCL\vendors\temp.txt
+
+for %%f in (%CONDA_PREFIX%\Library\etc\OpenCL\vendors\*.icd) do (
+  set /p icddllname=<%%f
+  echo|set /P =";%icddllname%" >> %CONDA_PREFIX%\Library\etc\OpenCL\vendors\temp.txt
 )
+
+set /p OCL_ICD_FILENAMES_NEW=<%CONDA_PREFIX%\Library\etc\OpenCL\vendors\temp.txt
+set "OCL_ICD_FILENAMES=%OCL_ICD_FILENAMES%%OCL_ICD_FILENAMES_NEW%"
+del %CONDA_PREFIX%\Library\etc\OpenCL\vendors\temp.txt
+
