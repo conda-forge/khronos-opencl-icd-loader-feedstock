5a9032c2e7d883351b6856008657e6fa1edf8974
Isuru Fernando <isuruf@gmail.com>
2021-03-31 10:59:12 -0500

    no need for shell

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -31,6 +31,4 @@ if errorlevel 1 exit 1
 for %%F in (activate deactivate) DO (
     if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
     copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
-    :: Copy unix shell activation scripts, needed by Windows Bash users
-    copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
 )
