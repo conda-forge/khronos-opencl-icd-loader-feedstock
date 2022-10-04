0535437b36bc0e9c6fde5f2e2286b69e0d9ac7e1
Isuru Fernando <isuruf@gmail.com>
2022-10-04 14:39:17 -0500

    activte.sh for mingw shells

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -31,6 +31,7 @@ if errorlevel 1 exit 1
 for %%F in (activate deactivate) DO (
     if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
     copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
 )
 
 mkdir %LIBRARY_PREFIX%\etc\OpenCL\vendors
